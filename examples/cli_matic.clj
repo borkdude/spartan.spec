f3179aff8cbed6ae06cf662c9b975768b804293d
Michiel Borkent <michielborkent@gmail.com>
2021-01-16 18:15:46 +0100

    Add cli-matic example

---
+++ examples/cli_matic.clj
@@ -0,0 +1,53 @@
+(ns java.text)
+(defrecord SimpleDateFormat [])
+
+(ns cli-matic
+  (:require [babashka.deps :as deps]))
+
+(deps/add-deps
+ '{:deps {borkdude/spartan.spec {:git/url "https://github.com/borkdude/spartan.spec"
+                                 :sha "bf4ace4a857c29cbcbb934f6a4035cfabe173ff1"}
+          cli-matic/cli-matic {:mvn/version "0.4.3"}}})
+
+(require 'spartan.spec) ;; creates clojure.spec.alpha
+
+(binding [*err* (java.io.StringWriter.)]
+  (require '[cli-matic.core :refer [run-cmd]]))
+
+;; again, to satisfy clj-kondo...
+(require '[cli-matic.core :refer [run-cmd]])
+
+(defn add_numbers
+  "Sums A and B together, and prints it in base `base`"
+  [{:keys [a1 a2 base]}]
+  (println
+   (Integer/toString (+ a1 a2) base)))
+
+(defn subtract_numbers
+  "Subtracts B from A, and prints it in base `base` "
+  [{:keys [pa pb base]}]
+  (println
+   (Integer/toString (- pa pb) base)))
+
+(def CONFIGURATION
+  {:app         {:command     "toycalc"
+                 :description "A command-line toy calculator"
+                 :version     "0.0.1"}
+   :global-opts [{:option  "base"
+                  :as      "The number base for output"
+                  :type    :int
+                  :default 10}]
+   :commands    [{:command     "add" :short "a"
+                  :description ["Adds two numbers together"
+                                ""
+                                "Looks great, doesn't it?"]
+                  :opts        [{:option "a1" :short "a" :env "AA" :as "First addendum" :type :int :default 0}
+                                {:option "a2" :short "b" :as "Second addendum" :type :int :default 0}]
+                  :runs        add_numbers}
+                 {:command     "sub"  :short "s"
+                  :description "Subtracts parameter B from A"
+                  :opts        [{:option "pa" :short "a" :as "Parameter A" :type :int :default 0}
+                                {:option "pb" :short "b" :as "Parameter B" :type :int :default 0}]
+                  :runs        subtract_numbers}]})
+
+(run-cmd *command-line-args* CONFIGURATION)
